214a5dafba5fa638e955ebbd614ae829f1eec2d4
Joris Heuberger <j0ris@bluewin.ch>
2006-03-02 13:01:17 +0000

    test commit from Xcode
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@5 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- VRView.mm
+++ VRView.mm
@@ -2305,6 +2305,7 @@ public:
 		}
 		else if( tool == tBoneRemoval)
 		{
+			// enable Undo
 			[controller prepareUndo];
 			// clicked point (2D coordinate)
 			mouseLocStart = [self convertPoint: [theEvent locationInWindow] fromView: 0L];
