1ce29ac79f486a4ffc5b3c18bf85c99aae4b909d
Arnaud Garcia <arnaud74130@users.sourceforge.net>
2006-03-09 16:02:43 +0000

    automatic frame Markers OK
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@82 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- MSRG/MSRGWindowController.h
+++ MSRG/MSRGWindowController.h
@@ -36,15 +36,17 @@ PURPOSE.
 	ViewerController *viewer;
 	ROI* BoundingROIStart;
 	ROI* BoundingROIEnd;
+	int begin,end;//for 3D ROI, FrameMarker ROI start at slice:begin to end 
 	
 }
 - (IBAction)startMSRG:(id)sender;
 - (IBAction)frameThicknessChange:(id)sender;
 - (IBAction)activateBoundingBox:(id)sender;
 - (IBAction)CreateDeleteMarkers:(id)sender;
-
+-(void)setThicknessParameters;
+-(void)createFrameMarker;
 - (id) initWithMarkerViewer:(ViewerController*) v andViewersList:(NSMutableArray*)list ;
 -(BOOL)checkBoundingBoxROIPresentOnCurrentSlice;
--(void)createMarkerROIWithWidth:(int)w andHeight:(int)h atPosX:(int)x andY:(int)y;
+-(void)createMarkerROIAtSlice:(int)slice Width:(int)w Height:(int)h PosX:(int)x PosY:(int)y;
 -(void) cleanStackFromMarkerFrame;
 @end
