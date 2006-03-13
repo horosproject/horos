856ecca2ed54f2343d3e70945d9dc67393b20d71
Lance Pysher <lpysher@users.sourceforge.net>
2006-03-13 04:26:40 +0000

    Subclassed Report Plugin. Changed report:Action to three methods createReport, deleteReport, reportDate
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@113 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ ReportPluginFilter.m
@@ -0,0 +1,36 @@
+//
+//  ReportPluginFilter.m
+//  OsiriX
+//
+//  Created by Lance Pysher on 3/12/06.
+
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://homepage.mac.com/rossetantoine/osirix/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
+
+#import "ReportPluginFilter.h"
+
+
+@implementation ReportPluginFilter
+
+- (BOOL)createReportForStudy:(id)study{
+	return NO;
+}
+- (BOOL)deleteReportForStudy:(id)study{
+	return NO;
+}
+- (NSDate *)reportDateForStudy:(id)study{
+	return nil;
+}
+
+@end
