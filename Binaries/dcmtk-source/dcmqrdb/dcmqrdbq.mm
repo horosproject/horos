586ef58f76bea69299107af9edc5df7532e0f06f
Lance Pysher <lpysher@users.sourceforge.net>
2006-03-21 23:49:25 +0000

    Q/R work
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@165 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- Binaries/dcmtk-source/dcmqrdb/dcmqrdbq.mm
+++ Binaries/dcmtk-source/dcmqrdb/dcmqrdbq.mm
@@ -18,7 +18,39 @@
      PURPOSE.
 =========================================================================*/
 
+#undef verify
+
+#include "osconfig.h"    /* make sure OS specific configuration is included first */
+
+BEGIN_EXTERN_C
+#ifdef HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif
+#ifdef HAVE_FCNTL_H
+#include <fcntl.h>
+#endif
+#ifdef HAVE_SYS_PARAM_H
+#include <sys/param.h>
+#endif
+END_EXTERN_C
+
+#define INCLUDE_CCTYPE
+#define INCLUDE_CSTDARG
+#include "ofstdinc.h"
+
+#include "dcmqrdbs.h"
+// #include "dcmqrdbi.h"
+#include "dcmqrcnf.h"
+
+#include "dcmqridx.h"
+#include "diutil.h"
+#include "dcfilefo.h"
+#include "ofstd.h"
+
+
 #import "dcmqrdbq.h"
+
+
 
 
 
