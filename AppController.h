9b55156271a5c05a64bf01640135415319f0a8c8
Jacques Fauquex <jacquesfauquex@users.sourceforge.net>
2006-03-23 13:14:49 +0000

    Reordered method list. Deleted duplicated declaration of cancelModal and okModal methods
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@170 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- AppController.h
+++ AppController.h
@@ -57,46 +57,54 @@ NSRect screenFrame();
 	NSMutableDictionary *currentHangingProtocol;
 	DCMNetServiceDelegate *dicomNetServiceDelegate;
 }
-
+#pragma mark-
+#pragma mark initialization of the main event loop singleton
 + (id) sharedAppController;
-
-
-- (IBAction) cancelModal: (id) sender;
-- (IBAction) okModal: (id) sender;
-- (IBAction) sendEmail: (id) sender;
-- (IBAction) openOsirixWebPage: (id) sender;
-- (IBAction) help: (id) sender;
-- (IBAction) openOsirixWikiWebPage: (id) sender;
-- (IBAction) openOsirixDiscussion: (id) sender;
-- (IBAction) openOsirixBugReporter: (id) sender;
-- (IBAction) openOsirixFeatureRequest: (id) sender;
-
-- (IBAction) closeAllViewers: (id) sender;
-- (IBAction) updateViews:(id) sender;
-- (IBAction) checkForUpdates:(id) sender;
-- (IBAction) showPreferencePanel:(id)sender;
-- (IBAction) about:(id)sender;
-- (void) startSTORESCP:(id) sender;
-- (void) tileWindows:(id)sender;
-- (NSScreen *)dbScreen;
-- (NSArray *)viewerScreens;
-- (void) restartSTORESCP;
-- (id) FindViewer:(NSString*) nib :(NSMutableArray*) pixList;
-- (NSArray*) FindRelatedViewers:(NSMutableArray*) pixList;
-- (void) setCurrentHangingProtocolForModality: (NSString*) modality description: (NSString*) description;
-- (NSDictionary*) currentHangingProtocol;
-
 - (void) terminate :(id) sender;
-- (IBAction) cancelModal:(id) sender;
-- (IBAction) okModal:(id) sender;
+- (void) restartSTORESCP;
+- (void) startSTORESCP:(id) sender;
 - (void) startDICOMBonjourSearch;
 
+#pragma mark-
+#pragma mark static menu items
+//===============OSIRIX========================
+- (IBAction) about:(id)sender;
+- (IBAction) showPreferencePanel:(id)sender;
+- (IBAction) checkForUpdates:(id) sender;
+//===============2D VIEWER=====================
+- ( NSMenuItem *)	syncSeriesMenuItem;
+//===============WINDOW========================
+- (void) tileWindows:(id)sender;
+- (IBAction) closeAllViewers: (id) sender;
+//===============HELP==========================
+- (IBAction) sendEmail: (id) sender;
+- (IBAction) openOsirixWebPage: (id) sender;
+- (IBAction) openOsirixDiscussion: (id) sender;
+//---------------------------------------------
+- (IBAction) help: (id) sender;
+- (IBAction) openOsirixWikiWebPage: (id) sender;
+- (IBAction) openOsirixBugReporter: (id) sender;
+- (IBAction) openOsirixFeatureRequest: (id) sender;
+//=============================================
+
+#pragma mark-
+#pragma mark window routines
+- (IBAction) updateViews:(id) sender;
+- (NSScreen *)dbScreen;
+- (NSArray *)viewerScreens;
+- (id) FindViewer:(NSString*) nib :(NSMutableArray*) pixList;
+- (NSArray*) FindRelatedViewers:(NSMutableArray*) pixList;
+- (IBAction) cancelModal: (id) sender;
+- (IBAction) okModal: (id) sender;
+
+#pragma mark-
+#pragma mark display setters and getters
+- (void) setCurrentHangingProtocolForModality: (NSString*) modality description: (NSString*) description;
+- (NSDictionary*) currentHangingProtocol;
 - (BOOL) xFlipped;
 - (void) setXFlipped: (BOOL) v;
 - (BOOL) yFlipped;
 - (void) setYFlipped: (BOOL) v;
-
-- ( NSMenuItem *)	syncSeriesMenuItem;
 
 #pragma mark-
 #pragma mark Geneva University Hospital (HUG) specific function
