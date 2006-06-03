ec8a92688acfa433175c823a4231567f97a412de
Antoine Rosset <rossetantoine@bluewin.ch>
2006-06-03 08:17:37 +0000

    refresh view
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@595 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- browserController.m
+++ browserController.m
@@ -1214,6 +1214,7 @@ static BOOL COMPLETEREBUILD = NO;
 		{
 			[self performSelectorOnMainThread:@selector( reloadViewers:) withObject:viewersListToReload waitUntilDone:YES];
 			[self performSelectorOnMainThread:@selector( rebuildViewers:) withObject:viewersListToRebuild waitUntilDone:YES];
+			[self performSelectorOnMainThread:@selector( outlineViewRefresh:) withObject:0L waitUntilDone:YES];
 			
 			databaseLastModification = [NSDate timeIntervalSinceReferenceDate];
 		}
