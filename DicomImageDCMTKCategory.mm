0325f29570999bf5d66efd980cfb67cef0880da8
Lance Pysher <lpysher@users.sourceforge.net>
2006-07-08 04:25:41 +0000

    Extract KO references
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@716 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- DicomImageDCMTKCategory.mm
+++ DicomImageDCMTKCategory.mm
@@ -28,7 +28,8 @@
 #include "dsrdoc.h"
 #include "dcuid.h"
 #include "dcfilefo.h"
-
+#include "dsrtypes.h"
+#include "dsrimgtn.h"
 
 @implementation DicomImage(DicomImageDCMTKCategory)
 
@@ -47,17 +48,28 @@
 	return type;
 }
 - (NSArray *)referencedObjects{
-	NSArray *references = nil;
+	NSMutableArray *references = [NSMutableArray array];
 	DcmFileFormat fileformat;
 	DSRDocument *doc = new DSRDocument();
 	OFCondition status = fileformat.loadFile([[self completePath] UTF8String]);
 	if (status.good())
 		status = doc->read(*fileformat.getDataset());
 	if (status.good()){
-		//go down one level from root. Can have descrption or referenced objects
-		doc->getTree().goDown();
-		
-		//get referenced images
+		DSRDocumentTreeNode *node = NULL; 
+		//DSRDocumentTree  *tree = doc->getTree();
+		/* iterate over all nodes */ 
+        do { 
+            node = OFstatic_cast(DSRDocumentTreeNode *, doc->getTree().getNode()); 
+            if (node->getValueType() == DSRTypes::VT_Image) {
+			//image node get SOPCInstance
+			DSRImageTreeNode *imageNode = OFstatic_cast(DSRImageTreeNode *, node);
+			OFString sopInstance = imageNode->getSOPInstanceUID();
+			NSString *uid = [NSString stringWithUTF8String:sopInstance.c_str()];
+			if (uid)
+				[references addObject:uid];
+			
+			}
+        } while (doc->getTree().iterate()); 
 	}
 	delete doc;
 	return references;
