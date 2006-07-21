95f4191d413b977961694b8842c067b698d45045
Joris Heuberger <j0ris@bluewin.ch>
2006-07-21 12:23:21 +0000

    QuickTime export. HTML: OsiriX copyright header in code.
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@821 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- QTExportHTMLSummary.h
+++ QTExportHTMLSummary.h
@@ -1,10 +1,16 @@
-//
-//  QTExportHTMLSummary.h
-//  OsiriX
-//
-//  Created by joris on 17/07/06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://homepage.mac.com/rossetantoine/osirix/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
 
 #import <Cocoa/Cocoa.h>
 
