54ccf87f5219507ec826d4d2b5f9a8c0087c743f
Antoine Rosset <rossetantoine@bluewin.ch>
2006-08-27 21:06:14 +0000

    64-bit work in progress
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@1002 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- NSSplitViewSave.m
+++ NSSplitViewSave.m
@@ -21,43 +21,54 @@
 
 - (void) restoreDefault: (NSString *) defaultName
 {
-        NSString * string = [[NSUserDefaults standardUserDefaults] objectForKey: defaultName];
+	NSString * string = [[NSUserDefaults standardUserDefaults] objectForKey: defaultName];
 
-        if (string == nil)
-                return;         // there was no saved default
+	if (string == nil)
+			return;         // there was no saved default
+	
+	NSScanner* scanner = [NSScanner scannerWithString: string];
+	NSRect r0, r1;
+	
+	float a,b,c,d,e,f,g,h,i,j;
 
-        NSScanner* scanner = [NSScanner scannerWithString: string];
-        NSRect r0, r1;
+	BOOL didScan =
+			[scanner scanFloat: &a]             &&
+			[scanner scanFloat: &b]             &&
+			[scanner scanFloat: &c]				&&
+			[scanner scanFloat: &d]				&&
+			[scanner scanFloat: &e]             &&
+			[scanner scanFloat: &f]             &&
+			[scanner scanFloat: &g]				&&
+			[scanner scanFloat: &h];
 
-        BOOL didScan =
-                [scanner scanFloat: &(r0.origin.x)]             &&
-                [scanner scanFloat: &(r0.origin.y)]             &&
-                [scanner scanFloat: &(r0.size.width)]   &&
-                [scanner scanFloat: &(r0.size.height)]  &&
-                [scanner scanFloat: &(r1.origin.x)]             &&
-                [scanner scanFloat: &(r1.origin.y)]             &&
-                [scanner scanFloat: &(r1.size.width)]   &&
-                [scanner scanFloat: &(r1.size.height)];
+	r0.origin.x = a;
+	r0.origin.y = b;
+	r0.size.width = c;
+	r0.size.height = d;
+	r1.origin.x = e;
+	r1.origin.y = f;
+	r1.size.width = g;
+	r1.size.height = h;
 
-        if (didScan == NO)
-                return; // probably should throw an exception at this point
+	if (didScan == NO)
+			return; // probably should throw an exception at this point
 
-        [[[self subviews] objectAtIndex: 0] setFrame: r0];
-        [[[self subviews] objectAtIndex: 1] setFrame: r1];
+	[[[self subviews] objectAtIndex: 0] setFrame: r0];
+	[[[self subviews] objectAtIndex: 1] setFrame: r1];
 
-        [self adjustSubviews];
+	[self adjustSubviews];
 }
 
 - (void) saveDefault: (NSString *) defaultName
 {
-        NSRect r0 = [[[self subviews] objectAtIndex: 0] frame];
-        NSRect r1 = [[[self subviews] objectAtIndex: 1] frame];
+	NSRect r0 = [[[self subviews] objectAtIndex: 0] frame];
+	NSRect r1 = [[[self subviews] objectAtIndex: 1] frame];
 
-        NSString * string = [NSString stringWithFormat: @"%f %f %f %f %f %f %f %f",
-                r0.origin.x, r0.origin.y, r0.size.width, r0.size.height,
-                r1.origin.x, r1.origin.y, r1.size.width, r1.size.height];
-
-        [[NSUserDefaults standardUserDefaults] setObject: string forKey: defaultName];
+	NSString * string = [NSString stringWithFormat: @"%f %f %f %f %f %f %f %f",
+			r0.origin.x, r0.origin.y, r0.size.width, r0.size.height,
+			r1.origin.x, r1.origin.y, r1.size.width, r1.size.height];
+	
+	[[NSUserDefaults standardUserDefaults] setObject: string forKey: defaultName];
 }
 
 @end
