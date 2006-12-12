4091bfb2c9f7763c6beb41b0d3e6b9ce0434a2ee
Lance Pysher <lpysher@users.sourceforge.net>
2006-12-12 04:38:00 +0000

    Added OSIWindowController. Will be super class for the Viewer WindowControllers in OsiriX
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@1579 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ OSIWindowController.m
@@ -0,0 +1,33 @@
+//
+//  OSIWindowController.m
+//  OsiriX
+//
+//  Created by Lance Pysher on 12/11/06.
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://homepage.mac.com/rossetantoine/osirix/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
+
+#import "OSIWindowController.h"
+
+
+@implementation OSIWindowController
+
+- (id)initWithWindowNibName:(NSString *)windowNibName{
+	if (self = [super initWithWindowNibName:(NSString *)windowNibName]) {
+	 // do what OsiriX needs to do for window Controllers
+	 // Register with WindowLayoutManager
+	}
+	return self;
+}
+
+@end
