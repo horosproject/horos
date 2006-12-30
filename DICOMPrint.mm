aee7bec56f945ae38cc80c8b77b561935dc320b1
Antoine Rosset <rossetantoine@bluewin.ch>
2006-12-30 21:44:10 +0000

    DICOMPrint as external process
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@1702 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ DICOMPrint.mm
@@ -0,0 +1,28 @@
+#import <Foundation/Foundation.h>
+#import <OsiriX/DCMObject.h>
+#import <OsiriX/DCMTransferSyntax.h>
+#import <OsiriX/DCMTransferSyntax.h>
+
+#include "AYDcmPrintSCU.h"
+
+int main(int argc, const char *argv[])
+{
+	NSAutoreleasePool	*pool	= [[NSAutoreleasePool alloc] init];
+	int status = -1;
+	
+	//	argv[ 1] : logPath
+	//	argv[ 2] : baseName
+	//	argv[ 3] : xmlPath
+	
+	if( argv[ 1] && argv[ 2] && argv[ 3])
+	{
+		// send printjob
+		AYDcmPrintSCU printSCU = AYDcmPrintSCU( argv[ 1], 0, argv[ 2]);
+		
+		status = printSCU.sendPrintjob( argv[ 3]);
+	}
+	
+	[pool release];
+	
+	return status;
+}
