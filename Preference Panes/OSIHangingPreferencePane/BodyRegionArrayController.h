d2fab75bca02653b4f896ad993e92135dcef55c0
Lance Pysher <lpysher@users.sourceforge.net>
2007-01-02 23:40:46 +0000

    Body Regions working
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@1716 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ Preference Panes/OSIHangingPreferencePane/BodyRegionArrayController.h
@@ -0,0 +1,30 @@
+//
+//  BodyRegionArrayController.h
+//  OSIHangingPreferencePane
+//
+//  Created by Lance Pysher on 1/2/07.
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://homepage.mac.com/rossetantoine/osirix/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
+
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BodyRegionArrayController : NSArrayController {
+
+}
+
+- (IBAction)addOrRemove:(id)sender;
+
+@end
---
+++ Preference Panes/OSIHangingPreferencePane/BodyRegionArrayController.m
@@ -0,0 +1,53 @@
+//
+//  BodyRegionArrayController.m
+//  OSIHangingPreferencePane
+//
+//  Created by Lance Pysher on 1/2/07.
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://homepage.mac.com/rossetantoine/osirix/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
+
+
+#import "BodyRegionArrayController.h"
+
+
+@implementation BodyRegionArrayController
+
+- (void)setContent:(id)content{
+	NSMutableArray *newContent = [NSMutableArray array];
+	NSDictionary *dict;
+	NSEnumerator *enumerator = [content objectEnumerator];
+	while (dict = [enumerator nextObject]){
+		NSMutableDictionary *newDict = [NSMutableDictionary dictionaryWithDictionary:dict];
+		NSEnumerator *enumerator2 = [[newDict objectForKey:@"keywords"] objectEnumerator];
+		NSDictionary *keywords;
+		NSMutableArray *childArray = [NSMutableArray array];
+		while (keywords = [enumerator2 nextObject]) {
+			NSMutableDictionary *newKeywords  = [NSMutableDictionary dictionaryWithDictionary:keywords];
+			[childArray addObject:newKeywords];
+		}
+		[newDict setObject:childArray forKey:@"keywords"];		
+		[newContent addObject:newDict];
+	}
+	[super setContent:newContent];
+}
+
+- (IBAction)addOrRemove:(id)sender{
+	if ([sender selectedSegment] == 0)
+		[self add:sender];
+	else
+		[self remove:sender];
+}
+	
+
+@end
