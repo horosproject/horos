9beb1e18e884a798a0feb4f9b326a033dbef74a2
Antoine Rosset <rossetantoine@bluewin.ch>
2007-02-25 08:51:58 +0000

    jpeg2000 memory leak
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@2067 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- browserController.m
+++ browserController.m
@@ -5735,9 +5735,9 @@ static BOOL withReset = NO;
 	NSData *imageData = [image  TIFFRepresentation];
 	
 	NSBitmapImageRep *imageRep = [NSBitmapImageRep imageRepWithData:imageData];
-	NSDictionary *imageProps = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:0.7] forKey:NSImageCompressionFactor];
+	NSDictionary *imageProps = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:0.6] forKey:NSImageCompressionFactor];
 	
-	NSData	*result = [imageRep representationUsingType:NSJPEG2000FileType properties:imageProps];
+	NSData	*result = [imageRep representationUsingType:NSJPEGFileType properties:imageProps];	//NSJPEGFileType	NSJPEG2000FileType <- MAJOR memory leak with NSJPEG2000FileType when reading !!!
 	
 	NSLog( @"thumbnail size: %d", [result length]);
 	
