2e09ce1752882a427a8efc97dfdef6ed930e821a
Antoine Rosset <rossetantoine@bluewin.ch>
2007-02-25 09:19:16 +0000

    jpeg2000 memory leak
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@2068 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- browserController.m
+++ browserController.m
@@ -5737,7 +5737,7 @@ static BOOL withReset = NO;
 	NSBitmapImageRep *imageRep = [NSBitmapImageRep imageRepWithData:imageData];
 	NSDictionary *imageProps = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:0.6] forKey:NSImageCompressionFactor];
 	
-	NSData	*result = [imageRep representationUsingType:NSJPEGFileType properties:imageProps];	//NSJPEGFileType	NSJPEG2000FileType <- MAJOR memory leak with NSJPEG2000FileType when reading !!!
+	NSData	*result = [imageRep representationUsingType:NSJPEGFileType properties:imageProps];	//NSJPEGFileType	NSJPEG2000FileType <- MAJOR memory leak with NSJPEG2000FileType when reading !!! Kakadu library...
 	
 	NSLog( @"thumbnail size: %d", [result length]);
 	
