259c68c6f7b076a07c20451964c6aaa058f65b3e
Antoine Rosset <rossetantoine@bluewin.ch>
2007-08-31 19:17:31 +0000

    soap server
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@2899 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ TCPServer.h
@@ -0,0 +1,66 @@
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://www.osirix-viewer.com/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
+
+#import <Foundation/Foundation.h>
+#import <CoreServices/CoreServices.h>
+
+//NSString * const TCPServerErrorDomain;
+
+typedef enum {
+    kTCPServerCouldNotBindToIPv4Address = 1,
+    kTCPServerCouldNotBindToIPv6Address = 2,
+    kTCPServerNoSocketsAvailable = 3,
+} TCPServerErrorCode;
+
+@interface TCPServer : NSObject {
+@private
+    id delegate;
+    NSString *domain;
+    NSString *name;
+    NSString *type;
+    uint16_t port;
+    CFSocketRef ipv4socket;
+    CFSocketRef ipv6socket;
+    NSNetService *netService;
+}
+
+- (id)delegate;
+- (void)setDelegate:(id)value;
+
+- (NSString *)domain;
+- (void)setDomain:(NSString *)value;
+
+- (NSString *)name;
+- (void)setName:(NSString *)value;
+
+- (NSString *)type;
+- (void)setType:(NSString *)value;
+
+- (uint16_t)port;
+- (void)setPort:(uint16_t)value;
+
+- (BOOL)start:(NSError **)error;
+- (BOOL)stop;
+
+- (void)handleNewConnectionFromAddress:(NSData *)addr inputStream:(NSInputStream *)istr outputStream:(NSOutputStream *)ostr;
+// called when a new connection comes in; by default, informs the delegate
+
+@end
+
+@interface TCPServer (TCPServerDelegateMethods)
+- (void)TCPServer:(TCPServer *)server didReceiveConnectionFromAddress:(NSData *)addr inputStream:(NSInputStream *)istr outputStream:(NSOutputStream *)ostr;
+// if the delegate implements this method, it is called when a new  
+// connection comes in; a subclass may, of course, change that behavior
+@end
+
