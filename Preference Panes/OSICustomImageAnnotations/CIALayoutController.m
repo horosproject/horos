2c125ac6d1e889a16253b979bd088c61afecfd43
Antoine Rosset <rossetantoine@bluewin.ch>
2007-09-14 20:48:27 +0000

    annotations dcmtk
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@2982 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- Preference Panes/OSICustomImageAnnotations/CIALayoutController.m
+++ Preference Panes/OSICustomImageAnnotations/CIALayoutController.m
@@ -12,7 +12,6 @@
      PURPOSE.
 =========================================================================*/
 
-#import "CIALayoutControllerDCMTK.h"
 #import "CIALayoutController.h"
 #import "CIADICOMField.h"
 
