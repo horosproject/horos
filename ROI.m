23d239429607f9755e79aa5b6bf629ac05cb5b85
Antoine Rosset <rossetantoine@bluewin.ch>
2007-11-15 16:05:13 +0000

    misc ROI
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@3558 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- ROI.m
+++ ROI.m
@@ -4889,10 +4889,10 @@ NSInteger sortPointArrayAlongX(id point1, id point2, void *context)
 	
 	// available only for ROI types : Open Polygon, Close Polygon, Pencil
 	// for other types, returns the original points
-	if(type!=tOPolygon && type!=tCPolygon && type!=tPencil) return points;
+	if(type!=tOPolygon && type!=tCPolygon && type!=tPencil) return [self points];
 	
 	// available only for polygons with at least 3 points
-	if([points count]<3) return points;
+	if([points count]<3) return [self points];
 	
 	int nb; // number of points
 	if(type==tOPolygon) nb = [points count];
