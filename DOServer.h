0b6d0dcafa5f8d4cca1dc54f4bf696c6ae045643
Antoine Rosset <rossetantoine@bluewin.ch>
2007-12-14 06:52:28 +0000

    DOServer
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@3676 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- DOServer.h
+++ DOServer.h
@@ -4,8 +4,8 @@
 {
          NSConnection* serverConnection;
 }
-- (id)log: (id)string;
-- (void)serve;
-- (NSConnection*) createConnectionName:(NSString*)name;
-- (void) bye;
+//- (id)log: (id)string;
+//- (void)serve;
+//- (NSConnection*) createConnectionName:(NSString*)name;
+//- (void) bye;
 @end
--- DOServer.m
+++ DOServer.m
@@ -5,40 +5,40 @@
 
 @implementation DOServer
 
-- (id)log: (id)string
-{
-	NSLog( string);
-	
-	return [NSMutableArray arrayWithObject: @"prout"];
-}
-
-- (void)bye
-{
-	NSLog( @"bye");
-	exit( 0);
-}
-
-- (void)serve
-{
-	serverConnection = [self createConnectionName:@"OsiriX_DistributedObjects_OsiriX"];
-
-	[[NSRunLoop currentRunLoop] run];
-}
-
-- (NSConnection*) createConnectionName:(NSString*)name
-{
-	NSConnection* newConnection=[[NSConnection alloc] init];
-	if ([newConnection registerName:name])
-	{
-		[newConnection setRootObject:self];
-	}
-	else
-	{
-		[newConnection release];
-		newConnection=nil;
-	}
-	
-	return newConnection;
-}
+//- (id)log: (id)string
+//{
+//	NSLog( string);
+//	
+//	return [NSMutableArray arrayWithObject: @"prout"];
+//}
+//
+//- (void)bye
+//{
+//	NSLog( @"bye");
+//	exit( 0);
+//}
+//
+//- (void)serve
+//{
+//	serverConnection = [self createConnectionName:@"OsiriX_DistributedObjects_OsiriX"];
+//
+//	[[NSRunLoop currentRunLoop] run];
+//}
+//
+//- (NSConnection*) createConnectionName:(NSString*)name
+//{
+//	NSConnection* newConnection=[[NSConnection alloc] init];
+//	if ([newConnection registerName:name])
+//	{
+//		[newConnection setRootObject:self];
+//	}
+//	else
+//	{
+//		[newConnection release];
+//		newConnection=nil;
+//	}
+//	
+//	return newConnection;
+//}
 
 @end
