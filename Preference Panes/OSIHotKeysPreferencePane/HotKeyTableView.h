ba054ebd6a1e9b64f419d7e6fca5adc1be3aae7c
Antoine Rosset <rossetantoine@bluewin.ch>
2008-02-04 11:22:40 +0000

    hot keys cleaning
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@3847 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- Preference Panes/OSIHotKeysPreferencePane/HotKeyTableView.h
+++ Preference Panes/OSIHotKeysPreferencePane/HotKeyTableView.h
@@ -1,10 +1,16 @@
-//
-//  HotKeyTableView.h
-//  OSIHotKeysPreferencePane
-//
-//  Created by antoinerosset on 02.02.08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://www.osirix-viewer.com/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
 
 #import <Cocoa/Cocoa.h>
 
