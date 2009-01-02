e3e04bdb38997a9d35255f676a0c88d2ab5c7cec
Jacques Fauquex <jacquesfauquex@users.sourceforge.net>
2009-01-02 12:16:10 +0000

    key image counter and selection shortcuts
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@4910 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- browserController.m
+++ browserController.m
@@ -9842,13 +9842,13 @@ static BOOL needToRezoom;
 	NSMutableArray		*viewerPix[ 200];
 	ViewerController	*movieController = nil;
 	ViewerController	*createdViewer = viewer;
-	
+	int					keyImageCount = 0;//JF20081019
 	@try
 	{
 		// NS_DURING (1) keyImages
 		
-		if( keyImages)
-		{
+		//JF20081019 if( keyImages)
+		//JF20081019{
 			NSMutableArray *keyImagesToOpenArray = [NSMutableArray array];
 			
 			for( NSArray *loadList in toOpenArray )
@@ -9863,7 +9863,12 @@ static BOOL needToRezoom;
 				
 				if( [keyImagesArray count] > 0)
 					[keyImagesToOpenArray addObject: keyImagesArray];
+				
+				keyImageCount += [keyImagesArray count];//JF20081019
 			}
+			
+		if ( keyImages)
+		{	//JF20081019
 			
 			if( [keyImagesToOpenArray count] > 0) toOpenArray = keyImagesToOpenArray;
 			else
@@ -10145,13 +10150,15 @@ static BOOL needToRezoom;
 								if( viewer )
 								{
 									//reuse of existing viewer
-									[viewer changeImageData:viewerPix[0] :filesAr :volumeData :NO];
+									//JF20081018 [viewer changeImageData:viewerPix[0] :filesAr :volumeData :NO];
+									[viewer changeImageData:viewerPix[0] :filesAr :volumeData :NO withKeyImageCount:@"MF"];
 									[viewer startLoadImageThread];
 								}
 								else
 								{
 									//creation of new viewer
-									createdViewer = [[ViewerController alloc] initWithPix:viewerPix[0] withFiles:filesAr withVolume:volumeData];
+									//JF20081018 createdViewer = [[ViewerController alloc] initWithPix:viewerPix[0] withFiles:filesAr withVolume:volumeData];
+									createdViewer = [[ViewerController alloc] initWithPix:viewerPix[0] withFiles:filesAr withVolume:volumeData withKeyImageCount:@"MF"];
 									[createdViewer showWindowTransition];
 									[createdViewer startLoadImageThread];
 								}		
@@ -10163,14 +10170,16 @@ static BOOL needToRezoom;
 								//multiframe == NO
 								if( viewer)
 								{
-									//reuse of existing viewer
-									[viewer changeImageData:viewerPix[0] :[NSMutableArray arrayWithArray:correspondingObjects] :volumeData :NO];
+									//reuse of existing viewer 
+									//JF20081018 [viewer changeImageData:viewerPix[0] :[NSMutableArray arrayWithArray:correspondingObjects] :volumeData :NO];
+									[viewer changeImageData:viewerPix[0] :[NSMutableArray arrayWithArray:correspondingObjects] :volumeData :NO withKeyImageCount:[NSString stringWithFormat:@"%d",keyImageCount]];//JF20081018
 									[viewer startLoadImageThread];
 								}
 								else
 								{
 									//creation of new viewer
-									createdViewer = [[ViewerController alloc] initWithPix:viewerPix[0] withFiles:[NSMutableArray arrayWithArray:correspondingObjects] withVolume:volumeData];
+									//JF20081018 createdViewer = [[ViewerController alloc] initWithPix:viewerPix[0] withFiles:[NSMutableArray arrayWithArray:correspondingObjects] withVolume:volumeData];
+									createdViewer = [[ViewerController alloc] initWithPix:viewerPix[0] withFiles:[NSMutableArray arrayWithArray:correspondingObjects] withVolume:volumeData withKeyImageCount:[NSString stringWithFormat:@"%d",keyImageCount]];//JF20081018
 									[createdViewer showWindowTransition];
 									[createdViewer startLoadImageThread];
 								}
