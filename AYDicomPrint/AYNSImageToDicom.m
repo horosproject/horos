7f336940323ca05cd221408b5ff237f09670384b
Antoine Rosset <rossetantoine@bluewin.ch>
2009-07-06 20:38:32 +0000

    tests
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@5737 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- AYDicomPrint/AYNSImageToDicom.m
+++ AYDicomPrint/AYNSImageToDicom.m
@@ -13,6 +13,7 @@
 =========================================================================*/
 
 #import "AYNSImageToDicom.h"
+#import "OSIWindowController.h"
 
 @interface AYNSImageToDicom (private)
 - (NSString *) _createDicomImageWithViewer: (ViewerController *) viewer toDestinationPath: (NSString *) destPath asColorPrint: (BOOL) colorPrint withAnnotations: (BOOL) annotations;
@@ -203,7 +204,16 @@
 //********************************************************************************************
 - (NSString *) _createDicomImageWithViewer: (ViewerController *) viewer toDestinationPath: (NSString *) destPath asColorPrint: (BOOL) colorPrint withAnnotations: (BOOL) annotations
 {
+//	NSRect r = [[viewer window] frame];
+//	BOOL m = [viewer magnetic];
+//	[viewer setMagnetic : NO];
+//	[[viewer window] setFrame: NSMakeRect(0, 0, r.size.width * 4, r.size.height * 4) display: NO];
+	
 	NSImage *currentImage = [[viewer imageView] nsimage: NO]; //NO=image sized by window
+	
+//	[[currentImage TIFFRepresentation] writeToFile: @"/test.tiff" atomically: YES];	
+//	[viewer setMagnetic : m];
+//	[[viewer window] setFrame: r display: NO];
 
 	NSDictionary *patientInfoDict = [self _getAnnotationDictionary: viewer];
 	
