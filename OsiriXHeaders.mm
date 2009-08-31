ef162d1c002a6a5f9c835981aef2fcaab5e30961
Alessandro Volz <alessandro.volz@gmail.com>
2009-08-31 06:35:51 +0000

    added dummy binary for Osirix Headers framework
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@5963 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ OsiriXHeaders.mm
@@ -0,0 +1,14 @@
+//
+//  OsiriXHeaders.mm
+//  OsiriX
+//
+//  Created by Alessandro Volz on 8/27/09.
+//  Copyright 2009 OsiriX Team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DummyClass : NSObject
+@end
+@implementation DummyClass
+@end
