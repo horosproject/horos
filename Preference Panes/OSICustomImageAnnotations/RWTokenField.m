accbecfb80002555766a4191e61ee061923a0e8b
Antoine Rosset <rossetantoine@bluewin.ch>
2009-12-10 19:56:38 +0000

    poseAsClass removed
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@6347 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- Preference Panes/OSICustomImageAnnotations/RWTokenField.m
+++ Preference Panes/OSICustomImageAnnotations/RWTokenField.m
@@ -28,33 +28,33 @@
 //***************************************************************************
 
 @implementation RWTokenFieldCell
-
-+ (void) load
-{
-	[RWTokenFieldCell poseAsClass:[NSTokenFieldCell class]];
-}
-
-- (void)_string:(id)fp8 tokenizeIndex:(int)fp12 inTextStorage:(id)fp16
-{
-	NSLog( @"*********** unsupported on 64-bit");
-	
-	[super _string:fp8 tokenizeIndex:fp12 inTextStorage:fp16];
-
-	if(![[self controlView] respondsToSelector:@selector(tokenFieldCellDidTokenizeString)]) return;
-	
-	[[self controlView] performSelector:@selector(tokenFieldCellDidTokenizeString) withObject:self];
-}
-
-- (void) setObjectValue:(id<NSCopying>)object
-{
-	NSLog( @"*********** unsupported on 64-bit");
-	
-	[super setObjectValue:object];
-	
-	if(![[self controlView] respondsToSelector:@selector(tokenFieldCellDidTokenizeString)]) return;
-	
-	[[self controlView] performSelector:@selector(tokenFieldCellDidTokenizeString) withObject:self];
-}
+//
+//+ (void) load
+//{
+//	[RWTokenFieldCell poseAsClass:[NSTokenFieldCell class]];
+//}
+//
+//- (void)_string:(id)fp8 tokenizeIndex:(int)fp12 inTextStorage:(id)fp16
+//{
+//	NSLog( @"*********** unsupported on 64-bit");
+//	
+//	[super _string:fp8 tokenizeIndex:fp12 inTextStorage:fp16];
+//
+//	if(![[self controlView] respondsToSelector:@selector(tokenFieldCellDidTokenizeString)]) return;
+//	
+//	[[self controlView] performSelector:@selector(tokenFieldCellDidTokenizeString) withObject:self];
+//}
+//
+//- (void) setObjectValue:(id<NSCopying>)object
+//{
+//	NSLog( @"*********** unsupported on 64-bit");
+//	
+//	[super setObjectValue:object];
+//	
+//	if(![[self controlView] respondsToSelector:@selector(tokenFieldCellDidTokenizeString)]) return;
+//	
+//	[[self controlView] performSelector:@selector(tokenFieldCellDidTokenizeString) withObject:self];
+//}
 
 @end
 
@@ -62,55 +62,55 @@
 
 @implementation RWTokenField
 
-+ (void) load
-{
-	[RWTokenField poseAsClass:[NSTokenField class]];
-}
-
-- (void) tokenFieldCellDidTokenizeString:(NSTokenFieldCell*)tokenFieldCell
-{
-	NSLog( @"*********** unsupported on 64-bit");
-	
-	NSDictionary* valueBindingInformation = [self infoForBinding:@"value"];
-	if(valueBindingInformation != nil)
-	{
-		id valueBindingObject = [valueBindingInformation objectForKey:NSObservedObjectKey];
-		NSString* valueBindingKeyPath = [valueBindingInformation objectForKey:NSObservedKeyPathKey];
-		
-		[valueBindingObject setValue:[self objectValue] forKeyPath:valueBindingKeyPath];
-	}
-	
-	[self sendAction:[self action] to:[self target]];
-
-	if([[self delegate] respondsToSelector:@selector(tokenFieldDidTokenizeString:)])
-	{
-		[[self delegate] performSelector:@selector(tokenFieldDidTokenizeString:) withObject:self];
-	}
-}
-
-- (void)concludeDragOperation:(id <NSDraggingInfo>)sender
-{
-	NSLog( @"*********** unsupported on 64-bit");
-	
-	NSLog(@"concludeDragOperation");
-	[super concludeDragOperation:sender];
-}
-
-- (void)draggingEnded:(id <NSDraggingInfo>)sender
-{
-	NSLog( @"*********** unsupported on 64-bit");
-	
-	NSLog(@"draggingEnded");
-	[super draggingEnded:sender];
-}
-
-- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
-{
-	NSLog( @"*********** unsupported on 64-bit");
-	
-	NSLog(@"performDragOperation");
-	return [super performDragOperation:sender];
-}
+//+ (void) load
+//{
+//	[RWTokenField poseAsClass:[NSTokenField class]];
+//}
+//
+//- (void) tokenFieldCellDidTokenizeString:(NSTokenFieldCell*)tokenFieldCell
+//{
+//	NSLog( @"*********** unsupported on 64-bit");
+//	
+//	NSDictionary* valueBindingInformation = [self infoForBinding:@"value"];
+//	if(valueBindingInformation != nil)
+//	{
+//		id valueBindingObject = [valueBindingInformation objectForKey:NSObservedObjectKey];
+//		NSString* valueBindingKeyPath = [valueBindingInformation objectForKey:NSObservedKeyPathKey];
+//		
+//		[valueBindingObject setValue:[self objectValue] forKeyPath:valueBindingKeyPath];
+//	}
+//	
+//	[self sendAction:[self action] to:[self target]];
+//
+//	if([[self delegate] respondsToSelector:@selector(tokenFieldDidTokenizeString:)])
+//	{
+//		[[self delegate] performSelector:@selector(tokenFieldDidTokenizeString:) withObject:self];
+//	}
+//}
+//
+//- (void)concludeDragOperation:(id <NSDraggingInfo>)sender
+//{
+//	NSLog( @"*********** unsupported on 64-bit");
+//	
+//	NSLog(@"concludeDragOperation");
+//	[super concludeDragOperation:sender];
+//}
+//
+//- (void)draggingEnded:(id <NSDraggingInfo>)sender
+//{
+//	NSLog( @"*********** unsupported on 64-bit");
+//	
+//	NSLog(@"draggingEnded");
+//	[super draggingEnded:sender];
+//}
+//
+//- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
+//{
+//	NSLog( @"*********** unsupported on 64-bit");
+//	
+//	NSLog(@"performDragOperation");
+//	return [super performDragOperation:sender];
+//}
 
 
 @end
