4911674a8548a7cfce045cb9a58ffb82345fa1ac
Antoine Rosset <rossetantoine@bluewin.ch>
2010-03-05 07:51:34 +0000

    check for update
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@6853 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- DefaultsOsiriX.m
+++ DefaultsOsiriX.m
@@ -783,7 +783,7 @@ static NSHost *currentHost = nil;
 	[defaultValues setObject: @"1" forKey: @"PREFERPAPYRUSFORCD"];
 	[defaultValues setObject: @"0" forKey: @"SINGLEPROCESS"];
 	[defaultValues setObject: @"0" forKey: @"AUTHENTICATION"];
-	[defaultValues setObject: @"1" forKey: @"CheckOsiriXUpdates2"];
+	[defaultValues setObject: @"1" forKey: @"CheckOsiriXUpdates3"];
 	[defaultValues setObject: @"1" forKey:@"MOUNT"];
 	[defaultValues setObject: @"1" forKey:@"UNMOUNT"];
 	[defaultValues setObject: @"1" forKey: @"UseDICOMDIRFileCD"];
