f5db184df95c53d6b40757a5376590b3c36cb958
Alessandro Volz <alessandro.volz@gmail.com>
2010-04-28 08:23:03 +0000

    file ownership comments
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@7060 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- PreferencesView.h
+++ PreferencesView.h
@@ -1,10 +1,16 @@
-//
-//  PreferencesView.h
-//  OsiriX
-//
-//  Created by Alessandro Volz on 4/21/10.
-//  Copyright 2010 OsiriX Team. All rights reserved.
-//
+/*=========================================================================
+ Program:   OsiriX
+ 
+ Copyright (c) OsiriX Team
+ All rights reserved.
+ Distributed under GNU - LGPL
+ 
+ See http://www.osirix-viewer.com/copyright.html for details.
+ 
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.
+ =========================================================================*/
 
 #import <PreferencePanes/PreferencePanes.h>
 
