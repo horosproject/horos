0f928614aba8f35e8994cedd83f32474378472fa
Joris Heuberger <j0ris@bluewin.ch>
2010-07-23 09:33:49 +0000

    Activity viewer: totally removed MenuMeters. Warning when using open source code: GPL is viral
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@7499 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- BrowserController+Activity.mm
+++ BrowserController+Activity.mm
@@ -15,8 +15,6 @@
 #import "BrowserController+Activity.h"
 #import "ThreadsManager.h"
 #import "ThreadCell.h"
-#import "MenuMeterCPUStats.h"
-#import "MenuMeterNetStats.h"
 #import <OsiriX Headers/NSImage+N2.h>
 #import <OsiriX Headers/N2Operators.h>
 #import <mach/mach_port.h>
@@ -37,9 +35,6 @@
 
 -(void)awakeActivity {
 //	tableView = [BrowserController currentBrowser].AtableView;
-//	cpuActiView = [BrowserController currentBrowser].AcpuActiView;
-//	hddActiView = [BrowserController currentBrowser].AhddActiView;
-//	netActiView = [BrowserController currentBrowser].AnetActiView;
 //	statusLabel = [BrowserController currentBrowser].AstatusLabel;
 	
 	[AtableView setDelegate: self];
@@ -144,112 +139,14 @@ const CGFloat greenHue = 1./3, redHue = 0, deltaHue = redHue-greenHue;
 
 -(void)updateStatsThread:(id)obj {
 	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-//	MenuMeterCPUStats* menuMeterCPUStats = [[MenuMeterCPUStats alloc] init];
-//	MenuMeterNetStats* menuMeterNetStats = [[MenuMeterNetStats alloc] init];
 	
 	#define historyLen 100
-//	CGFloat cpuCurrLoad = -1, netCurrLoad = -1, hddCurrLoad = -1;
-//	NSUInteger prevTotalRW = 0;
-//	CGFloat hddTimedDeltaRWs[historyLen];
-//	memset(hddTimedDeltaRWs, 0, sizeof(hddTimedDeltaRWs));
-//	NSTimeInterval previousTime = [NSDate timeIntervalSinceReferenceDate];
-	
-//	mach_port_t masterPort;
-//	IOMasterPort(MACH_PORT_NULL, &masterPort);
-	
-//	NSImage* cpuImage = [NSImage imageNamed:@"activity_cpu.png"];
-//	NSImage* netImage = [NSImage imageNamed:@"activity_net.png"];
-//	NSImage* hddImage = [NSImage imageNamed:@"activity_hdd.png"];
 	
 	while (![[NSThread currentThread] isCancelled]) {
 		[NSThread sleepForTimeInterval:0.5];
 
 		NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-		//NSTimeInterval thisTime = [NSDate timeIntervalSinceReferenceDate], thisInterval = thisTime - previousTime;
-		/*
-		// CPU
-		
-		NSArray* cpuLoads = [menuMeterCPUStats currentLoad];
-		if (cpuLoads) {
-			CGFloat meanload = 0, maxload = 0;
-			for (NSDictionary* cpuLoad in cpuLoads) {
-				CGFloat thisLoad = 0;
-				for (NSString* key in cpuLoad)
-					thisLoad += [[cpuLoad objectForKey:key] floatValue]/cpuLoad.count;
-				meanload += thisLoad/cpuLoads.count;
-				maxload = std::max(maxload, thisLoad);
-			}
-			CGFloat load = meanload+maxload*10;//(meanload+maxload)/2;
-			if (fabs(cpuCurrLoad-load) > 0.01)
-			{
-				[AcpuActiView setImage:[BrowserController cpuActivityImage:cpuImage meanLoad:meanload maxLoad:maxload]];
-				[AcpuActiView performSelectorOnMainThread:@selector(setNeedsDisplay:) withObject:[NSNumber numberWithBool:YES] waitUntilDone:NO];
-				cpuCurrLoad = load;
-			}
-		} else
-			[AcpuActiView setImage:NULL]; // TO DO maybe: grayed image
-		
-		// NET
-		
-		NSDictionary* netLoads = [menuMeterNetStats netStatsForInterval:1];
-		if (netLoads) {
-			CGFloat totpeak = 0, totdeltain = 0;
-			for (NSString* key in netLoads) {
-				totpeak += [[[netLoads objectForKey:key] objectForKey:@"peak"] floatValue];
-				totdeltain += [[[netLoads objectForKey:key] objectForKey:@"deltain"] floatValue];;
-			} CGFloat load = totdeltain/totpeak;
-			if (fabs(netCurrLoad-load) > 0.01)
-			{
-				[AnetActiView setImage:[netImage imageWithHue: deltaHue*load]];
-				[AnetActiView performSelectorOnMainThread:@selector(setNeedsDisplay:) withObject:[NSNumber numberWithBool:YES] waitUntilDone:NO];
-				netCurrLoad = load;
-			}
-		} else
-			[AnetActiView setImage:NULL]; // TO DO maybe: grayed image
-		
-		// HDD
-		
-		NSUInteger totalRW = 0;
-		io_iterator_t blockDeviceIterator;
-		if (IOServiceGetMatchingServices(masterPort, IOServiceMatching(kIOBlockStorageDriverClass), &blockDeviceIterator) == KERN_SUCCESS) {
-			io_registry_entry_t driveEntry = MACH_PORT_NULL;
-			while ((driveEntry = IOIteratorNext(blockDeviceIterator))) {
-				CFDictionaryRef statistics = (CFDictionaryRef)IORegistryEntryCreateCFProperty(driveEntry, CFSTR(kIOBlockStorageDriverStatisticsKey), kCFAllocatorDefault, kNilOptions);
-				if (statistics) {
-					NSNumber* statNumber = (NSNumber*)[(NSDictionary*)statistics objectForKey:(NSString*)CFSTR(kIOBlockStorageDriverStatisticsBytesReadKey)];
-					if (statNumber)
-						totalRW += [statNumber unsignedLongLongValue];
-					statNumber = (NSNumber*)[(NSDictionary*)statistics objectForKey:(NSString*)CFSTR(kIOBlockStorageDriverStatisticsBytesWrittenKey)];
-					if (statNumber)
-						totalRW += [statNumber unsignedLongLongValue];
-					CFRelease(statistics);
-				}
-			}
-			IOObjectRelease(blockDeviceIterator);
-		}
-		if (totalRW) {
-			if (prevTotalRW) {
-				memcpy(&hddTimedDeltaRWs[0], &hddTimedDeltaRWs[1], sizeof(CGFloat)*(historyLen-1));
-				NSUInteger thisDeltaRW = totalRW-prevTotalRW;
-				hddTimedDeltaRWs[historyLen-1] = 1.*thisDeltaRW/thisInterval;
-				
-				CGFloat maxTimedDeltaRW = 0;
-				for (size_t i = 0; i < historyLen; ++i)
-					maxTimedDeltaRW = std::max(maxTimedDeltaRW, hddTimedDeltaRWs[i]);
-				
-				CGFloat load = hddTimedDeltaRWs[historyLen-1]/maxTimedDeltaRW;
-				
-				if (fabs(hddCurrLoad-load) > 0.01)
-				{
-					[AhddActiView setImage:[hddImage imageWithHue: deltaHue*load]];
-					[AhddActiView performSelectorOnMainThread:@selector(setNeedsDisplay:) withObject:[NSNumber numberWithBool:YES] waitUntilDone:NO];
-					hddCurrLoad = load;
-				}
-			}
-			
-			prevTotalRW = totalRW;			
-		} else [AhddActiView setImage:NULL];
-		*/
+
 		int threadCount = [ThreadsManager defaultManager].threads.count;
 		NSString *activityString = @"";
 		if (threadCount>0)
@@ -258,12 +155,9 @@ const CGFloat greenHue = 1./3, redHue = 0, deltaHue = redHue-greenHue;
 		}
 		[AstatusLabel performSelectorOnMainThread:@selector(setStringValue:) withObject:activityString waitUntilDone:YES];
 		
-		//previousTime = thisTime;
 		[pool release];
 	}
 	
-//	[menuMeterNetStats release];
-//	[menuMeterCPUStats release];
 	[pool release];
 }
 
