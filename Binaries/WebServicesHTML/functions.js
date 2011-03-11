b37d62fb1037399e07ac00cef90a6cc3c14c17c7
Alessandro Volz <alessandro.volz@gmail.com>
2011-03-11 10:48:25 +0000

    html fix, cd prefs to write weasis
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@8258 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ Binaries/WebServicesHTML/functions.js
@@ -0,0 +1,18 @@
+/* http://www.dustindiaz.com/getelementsbyclass/ */
+function getElementsByClass(searchClass,node,tag) {
+	var classElements = new Array();
+	if ( node == null )
+		node = document;
+	if ( tag == null )
+		tag = '*';
+	var els = node.getElementsByTagName(tag);
+	var elsLen = els.length;
+	var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+	for (i = 0, j = 0; i < elsLen; i++) {
+		if ( pattern.test(els[i].className) ) {
+			classElements[j] = els[i];
+			j++;
+		}
+	}
+	return classElements;
+}
