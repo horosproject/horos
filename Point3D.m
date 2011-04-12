443134955247dbf3883f3c333aa426d140216d93
Joel Spaltenstein <spalte@gmail.com>
2011-04-12 08:53:13 +0000

    moved 3DPoint N3 category from N3Geometry to 3DPoint
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@8398 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- Point3D.m
+++ Point3D.m
@@ -118,3 +118,32 @@
 }
 
 @end
+
+@implementation Point3D (N3GeometryAdditions)
+
++ (id)pointWithN3Vector:(N3Vector)vector
+{
+	return [[[Point3D alloc] initWithN3Vector:vector] autorelease];
+}
+
+- (id)initWithN3Vector:(N3Vector)vector
+{
+	if ( (self = [super init]) ) {
+		self.x = vector.x;
+		self.y = vector.y;
+		self.z = vector.z;
+	}
+	return self;
+}
+
+- (N3Vector)N3VectorValue
+{
+	N3Vector vector;
+	vector.x = self.x;
+	vector.y = self.y;
+	vector.z = self.z;
+	return vector;
+}
+
+@end
+
