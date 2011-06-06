f61db26bf8becaff596a04192db457760b83733a
Antoine Rosset <rossetantoine@bluewin.ch>
2011-06-06 15:06:09 +0000

    toolbar: customize bug corrected
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@8603 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- OsiriXToolbar.m
+++ OsiriXToolbar.m
@@ -15,15 +15,44 @@
 #import "OsiriXToolbar.h"
 #import "ViewerController.h"
 #import "ToolbarPanel.h"
+#import "NSWindow+N2.h"
 
-extern  ToolbarPanelController  *toolbarPanel[ 10];
-extern  BOOL					USETOOLBARPANEL;
+extern  ToolbarPanelController *toolbarPanel[ 10];
+extern  BOOL USETOOLBARPANEL;
 
 @implementation OsiriXToolbar
 
+- (void) checkIfCustomizationIsRunning:(NSTimer*) timer
+{
+	if( [self customizationPaletteIsRunning] == NO)
+	{
+		for( ViewerController *v in [ViewerController getDisplayed2DViewers])
+			[[v window] safelySetMovable: YES];
+		
+		for( int i = 0 ; i < 10; i++)
+		{
+			if( [toolbarPanel[ i] toolbar] == self)
+				[[toolbarPanel[ i] window] setLevel: NSNormalWindowLevel];
+		}
+		
+		[timer invalidate];
+	}
+}
+
 - (void)runCustomizationPalette:(id)sender
 {
+	for( ViewerController *v in [ViewerController getDisplayed2DViewers])
+		[[v window] safelySetMovable: NO];
+	
+	for( int i = 0 ; i < 10; i++)
+	{
+		if( [toolbarPanel[ i] toolbar] == self)
+			[[toolbarPanel[ i] window] setLevel: NSFloatingWindowLevel];
+	}
+	
 	[super runCustomizationPalette: sender];
+	
+	[NSTimer scheduledTimerWithTimeInterval: 0.3 target: self selector: @selector( checkIfCustomizationIsRunning:) userInfo: nil repeats: YES];
 }
 
 @end
