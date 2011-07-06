2f7822bb78df7514f549e8eb76b6c8f47f098f21
Antoine Rosset <rossetantoine@bluewin.ch>
2011-07-06 08:55:13 +0000

    DICOM Listener: dont decompress files, by default
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@8683 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- DefaultsOsiriX.m
+++ DefaultsOsiriX.m
@@ -954,7 +954,7 @@ static NSHost *currentHost = nil;
 	[defaultValues setObject: @"0" forKey: @"useSeriesDescription"];
 	[defaultValues setObject: @"1" forKey: @"combineProjectionSeries"];
 	[defaultValues setObject: @"0" forKey: @"combineProjectionSeriesMode"];
-	[defaultValues setObject: @"1" forKey: @"ListenerCompressionSettings"];
+	[defaultValues setObject: @"0" forKey: @"ListenerCompressionSettings"];
 	[defaultValues setObject: @"localizer,scout" forKey: @"NOLOCALIZER_Strings"];
 	
 	//hot key prefs
