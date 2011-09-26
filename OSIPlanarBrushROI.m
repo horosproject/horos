6ba4ad1926cd9f22057a6126ac0ceaf179a9ff65
Joel Spaltenstein <spalte@gmail.com>
2011-09-26 11:02:15 +0000

    added OSIPlanarBrushROI
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@8998 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ OSIPlanarBrushROI.m
@@ -0,0 +1,23 @@
+//
+//  OSIPlanarBrushROI.m
+//  OsiriX_Lion
+//
+//  Created by Joël Spaltenstein on 9/26/11.
+//  Copyright 2011 OsiriX Team. All rights reserved.
+//
+
+#import "OSIPlanarBrushROI.h"
+
+@implementation OSIPlanarBrushROI
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
