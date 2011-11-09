1a5b36d13065a3e93cc7a9469b12d28bca47f151
Antoine Rosset <rossetantoine@bluewin.ch>
2011-11-09 12:02:41 +0000

    WebServer: limit to 10 links displayed when multiple pages are available
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@9110 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- Binaries/WebServicesHTML/functions.js
+++ Binaries/WebServicesHTML/functions.js
@@ -16,3 +16,85 @@ function getElementsByClass(searchClass,node,tag) {
 	}
 	return classElements;
 }
+
+function printPagesHTML( numberOfPages, currentPage, parameters)
+{
+        	if( numberOfPages > 1)
+        	{
+        		document.write("Pages:  ");
+        		
+				var i=0;
+	            var previous=0;
+	            previous = currentPage-1;
+	            if( currentPage == 0) document.write("<<  ");
+	            else document.write("<b><a href='?"+parameters+"&page="+previous+"'><<  </a></b>");
+                
+                var start = 1;
+                var end = numberOfPages+1;
+                
+                if( end - start > 10)
+                {
+                    start = currentPage - 3;
+                    end = currentPage + 6;
+                }
+                
+                if( start < 1)
+                {
+                    end += (-start+1);
+                    if( end > numberOfPages+1)
+                        end = numberOfPages+1;
+                    
+                    start = 1;
+                }
+                
+                if( end > numberOfPages+1)
+                {
+                    start -= end - numberOfPages;
+                    if( start < 1)
+                        start = 1;
+                    
+                    end = numberOfPages+1;
+                }
+                
+                if( start == 2)
+                    start = 1;
+                
+                if( end == numberOfPages)
+                    end = numberOfPages+1;
+                
+                if( start != 1)
+                {
+                    var p = 1;
+                    
+                    if( 1 == currentPage+1) document.write("<b><u>"+p+"</u></b>  ");
+                	else document.write("<a href='?"+parameters+"&page="+0+"'>"+1+"</a>  ");
+                    
+                    document.write("  ...  ");
+                }
+                
+   	         	for (i=start;i<end;i++)
+    	        {
+        	    	var page = i-1;
+            		if( i == currentPage+1) document.write("<b><u>"+i+"</u></b>  ");
+                	else document.write("<a href='?"+parameters+"&page="+page+"'>"+i+"</a>  ");
+            	}
+                
+                if( end != numberOfPages+1)
+                {
+                    document.write("  ...  ");
+                    
+                    var p = 0;
+                    
+                    p = numberOfPages;
+                    pp = p-1;
+                    
+                    if( numberOfPages == currentPage+1) document.write("<b><u>"+p+"</u></b>  ");
+                	else document.write("<a href='?"+parameters+"&page="+pp+"'>"+p+"</a>  ");
+                }
+                
+           	 	var next=0;
+           	 	next = currentPage+1;
+           		if( next >= numberOfPages) document.write("  >>");
+            	else document.write("<b><a href='?"+parameters+"&page="+next+"'>  >></a></b>");
+            }
+}
