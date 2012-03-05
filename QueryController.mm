963d2afd498fe787b83288582159974ce6027af0
Alessandro Volz <alessandro.volz@gmail.com>
2012-03-05 07:56:23 +0000

    singular/plural strings
    git-svn-id: https://svn.code.sf.net/p/osirix/code/branches/aless_coredata@9481 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- QueryController.mm
+++ QueryController.mm
@@ -38,6 +38,7 @@
 #import "DicomDatabase.h"
 #import "DicomStudy.h"
 #import "CIADICOMField.h"
+#import "N2Stuff.h"
 
 #include "osconfig.h"
 #include "mdfconen.h"
@@ -2365,13 +2366,9 @@ extern "C"
 			
 			NSThread *t = [[[NSThread alloc] initWithTarget:self selector:@selector( performRetrieve:) object: selectedItems] autorelease];
 			t.name = NSLocalizedString( @"Retrieving images...", nil);
-			if( [selectedItems count] == 1)
-				t.status = [NSString stringWithFormat: NSLocalizedString( @"%d study", nil), [selectedItems count]];
-			else
-				t.status = [NSString stringWithFormat: NSLocalizedString( @"%d studies", nil), [selectedItems count]];
-			
-			if( [selectedItems count] > 1)
-				t.progress = 0;
+            t.status = N2LocalizedSingularPluralCount(selectedItems.count, @"study", @"studies");
+            if ([selectedItems count] > 1)
+                t.progress = 0;
 			t.supportsCancel = YES;
 			[[ThreadsManager defaultManager] addThreadAndStart: t];
 			
@@ -2672,14 +2669,9 @@ extern "C"
 				
 				NSThread *t = [[[NSThread alloc] initWithTarget:self selector:@selector( performRetrieve:) object: selectedItems] autorelease];
 				t.name = NSLocalizedString( @"Retrieving images...", nil);
-				
-				if( [selectedItems count] == 1)
-					t.status = [NSString stringWithFormat: NSLocalizedString( @"%d study", nil), [selectedItems count]];
-				else
-					t.status = [NSString stringWithFormat: NSLocalizedString( @"%d studies", nil), [selectedItems count]];
-				
-				if( [selectedItems count] > 1)
-					t.progress = 0;
+                t.status = N2LocalizedSingularPluralCount(selectedItems.count, @"study", @"studies");
+                if ([selectedItems count] > 1)
+                    t.progress = 0;
 				
 				t.supportsCancel = YES;
 				[[ThreadsManager defaultManager] addThreadAndStart: t];
