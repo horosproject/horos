1e58587506f766d872426494fa67116eb6dc0a0c
Antoine Rosset <rossetantoine@bluewin.ch>
2012-05-04 17:05:02 +0000

    Trying to find a solution to a deadlock
    git-svn-id: https://svn.code.sf.net/p/osirix/code/branches/aless_coredata@9781 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- DicomDatabase.mm
+++ DicomDatabase.mm
@@ -184,35 +184,41 @@ static NSMutableDictionary* databasesDictionary = [[NSMutableDictionary alloc] i
 		}
 }
 
--(oneway void)release {
+-(oneway void)release
+{
 	NSInteger prc;
-	@synchronized(self) {
+	@synchronized(self)
+    {
 		prc = self.retainCount;
 //		if (prc <= 2)
 //			NSLog(@"%@ - [DicomDatabase release] self.rc = %d, managedObjectContext.rc = %d ", self.name, prc, self.managedObjectContext.retainCount); 
-		[super release];
-	}
-	
+		
 	NSInteger rc = prc-1;
 //	if (rc == 1)
 //		NSLog(@"\tself.rc = %d, managedObjectContext.rc = %d ", self.retainCount, self.managedObjectContext.retainCount);
 //	if (rc == 0)
 //		NSLog(@"\tself.rc = 0, zombies arising..?");
 	
-	@synchronized (databasesDictionary) {
-		if (rc == 1 && [databasesDictionary keyForObject:self]) {
-//			NSLog(@"\tThis database's retainCount has gone down to 1; the context has %d registered objects", self.managedObjectContext.registeredObjects.count);
+        @synchronized (databasesDictionary)
+        {
+            if (rc == 1 && [databasesDictionary keyForObject:self])
+            {
+//              NSLog(@"\tThis database's retainCount has gone down to 1; the context has %d registered objects", self.managedObjectContext.registeredObjects.count);
 
-			//[managedObjectContext invalidate];
-				
-			if (self.managedObjectContext.retainCount /*- self.managedObjectContext.registeredObjects.count*/ == 1) {
-//				NSLog(@"\t\tThe context seems to be retained only by the database and by its registered objects.. We can release the database!");
-                id key = [databasesDictionary keyForObject:self];
-                if (key) {
-                    [[self class] performSelectorOnMainThread:@selector(_mainthreadDatabasesDictionaryRemoveObjectForKey:) withObject:key waitUntilDone:NO];
+                //[managedObjectContext invalidate];
+                    
+                if (self.managedObjectContext.retainCount /*- self.managedObjectContext.registeredObjects.count*/ == 1)
+                {
+//                  NSLog(@"\t\tThe context seems to be retained only by the database and by its registered objects.. We can release the database!");
+                    id key = [databasesDictionary keyForObject:self];
+                    if (key)
+                    {
+                        [[self class] performSelectorOnMainThread:@selector(_mainthreadDatabasesDictionaryRemoveObjectForKey:) withObject:key waitUntilDone:NO];
+                    }
                 }
-			}
-		}
+            }
+        }
+        [super release];
 	}
 }
 
