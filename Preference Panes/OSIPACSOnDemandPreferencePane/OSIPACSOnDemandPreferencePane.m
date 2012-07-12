5ad7ed68bdbf9621f2e33ed6d25c35b56a5e7f4f
Antoine Rosset <rossetantoine@bluewin.ch>
2012-07-12 06:31:35 +0000

    OSIPACSOnDemand preference pane
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@10084 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

---
+++ Preference Panes/OSIPACSOnDemandPreferencePane/OSIPACSOnDemandPreferencePane.m
@@ -0,0 +1,50 @@
+/*=========================================================================
+  Program:   OsiriX
+
+  Copyright (c) OsiriX Team
+  All rights reserved.
+  Distributed under GNU - GPL
+  
+  See http://www.osirix-viewer.com/copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+=========================================================================*/
+
+#import "OSIPACSOnDemandPreferencePane.h"
+
+@implementation OSIPACSOnDemandPreferencePane
+
+- (id) initWithBundle:(NSBundle *)bundle
+{
+	if( self = [super init])
+	{
+		NSNib *nib = [[NSNib alloc] initWithNibNamed: @"OSIPACSOnDemandPreferencePanePref" bundle: nil];
+		[nib instantiateNibWithOwner:self topLevelObjects: nil];
+		
+		[self setMainView: [mainWindow contentView]];
+		[self mainViewDidLoad];
+	}
+	
+	return self;
+}
+
+
+- (void) willUnselect
+{
+	[[[self mainView] window] makeFirstResponder: nil];
+}
+
+- (void) dealloc
+{
+	NSLog(@"dealloc OSIPACSOnDemandPreferencePane");
+	
+	[super dealloc];
+}
+
+- (void) mainViewDidLoad
+{
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+}
+@end
