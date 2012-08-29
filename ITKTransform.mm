bef55160d6326f551caacfee62f31374500ea68c
Antoine Rosset <rossetantoine@bluewin.ch>
2012-08-29 06:58:20 +0000

    ITK: catch obj-c and C++ exceptions
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@10342 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- ITKTransform.mm
+++ ITKTransform.mm
@@ -26,6 +26,7 @@
 #import "AppController.h"
 #import "ViewerController.h"
 #import "DCMPix.h"
+#import "N2Debug.h"
 
 typedef itk::ResampleImageFilter<ImageType, ImageType> ResampleFilterType;
 
@@ -55,79 +56,94 @@ typedef itk::ResampleImageFilter<ImageType, ImageType> ResampleFilterType;
 
 	[firstObject orientationDouble: vectorReference];
 	[firstObjectOriginal orientationDouble: vectorOriginal];
+    
+    WaitRendering *splash = nil;
+    float* resultBuff = nil;
+    
+    try
+    {
+        @try
+        {
+            typedef itk::AffineTransform< double, 3 > AffineTransformType;
+            typedef AffineTransformType::ParametersType ParametersType;
+            
+            AffineTransformType::Pointer transform = AffineTransformType::New();
+            
+            ParametersType parameters(transform->GetNumberOfParameters());
+            
+            int i;
+            for(i=0; i<transform->GetNumberOfParameters(); i++)
+            {
+                parameters[i] = theParameters[i];
+            }
+            
+            transform->SetParameters(parameters);
 
-	typedef itk::AffineTransform< double, 3 > AffineTransformType;
-	typedef AffineTransformType::ParametersType ParametersType;
-	
-	AffineTransformType::Pointer transform = AffineTransformType::New();
-	
-	ParametersType parameters(transform->GetNumberOfParameters());
-	
-	int i;
-	for(i=0; i<transform->GetNumberOfParameters(); i++)
-	{
-		parameters[i] = theParameters[i];
-	}
-	
-	transform->SetParameters(parameters);
+            double origin[ 3] = {0, 0, 0}, originConverted[ 3];
+            
+            origin[0] =  [firstObjectOriginal originX];
+            origin[1] =  [firstObjectOriginal originY];
+            origin[2] =  [firstObjectOriginal originZ];
+            
+            originConverted[ 0] = origin[ 0] * vectorOriginal[ 0] + origin[ 1] * vectorOriginal[ 1] + origin[ 2] * vectorOriginal[ 2];
+            originConverted[ 1] = origin[ 0] * vectorOriginal[ 3] + origin[ 1] * vectorOriginal[ 4] + origin[ 2] * vectorOriginal[ 5];
+            originConverted[ 2] = origin[ 0] * vectorOriginal[ 6] + origin[ 1] * vectorOriginal[ 7] + origin[ 2] * vectorOriginal[ 8];
 
-	double origin[ 3] = {0, 0, 0}, originConverted[ 3];
-	
-	origin[0] =  [firstObjectOriginal originX];
-	origin[1] =  [firstObjectOriginal originY];
-	origin[2] =  [firstObjectOriginal originZ];
-	
-	originConverted[ 0] = origin[ 0] * vectorOriginal[ 0] + origin[ 1] * vectorOriginal[ 1] + origin[ 2] * vectorOriginal[ 2];
-	originConverted[ 1] = origin[ 0] * vectorOriginal[ 3] + origin[ 1] * vectorOriginal[ 4] + origin[ 2] * vectorOriginal[ 5];
-	originConverted[ 2] = origin[ 0] * vectorOriginal[ 6] + origin[ 1] * vectorOriginal[ 7] + origin[ 2] * vectorOriginal[ 8];
+            [itkImage itkImporter]->SetOrigin( originConverted );
 
-	[itkImage itkImporter]->SetOrigin( originConverted );
-
-	ResampleFilterType::Pointer resample = ResampleFilterType::New();
-	
-	resample->SetTransform(transform);
-	resample->SetInput([itkImage itkImporter]->GetOutput());
-	resample->SetDefaultPixelValue( [firstObjectOriginal minValueOfSeries]);
-	
-	double outputSpacing[3];
-	outputSpacing[0] = [firstObject pixelSpacingX];
-	outputSpacing[1] = [firstObject pixelSpacingY];
-	outputSpacing[2] = [firstObject sliceInterval];
-	
-	if( outputSpacing[2] == 0 || noOfImages == 1)
-		outputSpacing[2] = 1;
-	
-	resample->SetOutputSpacing(outputSpacing);
-	
-	double outputOrigin[3] = {0, 0, 0}, outputOriginConverted[3] = {0, 0, 0};
-	
-	outputOrigin[0] =  [firstObject originX];
-	outputOrigin[1] =  [firstObject originY];
-	outputOrigin[2] =  [firstObject originZ];
-	
-	outputOriginConverted[ 0] = outputOrigin[ 0] * vectorReference[ 0] + outputOrigin[ 1] * vectorReference[ 1] + outputOrigin[ 2] * vectorReference[ 2];
-	outputOriginConverted[ 1] = outputOrigin[ 0] * vectorReference[ 3] + outputOrigin[ 1] * vectorReference[ 4] + outputOrigin[ 2] * vectorReference[ 5];
-	outputOriginConverted[ 2] = outputOrigin[ 0] * vectorReference[ 6] + outputOrigin[ 1] * vectorReference[ 7] + outputOrigin[ 2] * vectorReference[ 8];
-	
-	resample->SetOutputOrigin( outputOriginConverted);
-	
-	ImageType::SizeType size;
-	size[0] = [firstObject pwidth];
-	size[1] = [firstObject pheight];
-	size[2] = noOfImages;
-	
-	resample->SetSize(size);
-	
-	WaitRendering *splash = nil;
-	
-	if( noOfImages > 2)
-		splash = [[WaitRendering alloc] init:NSLocalizedString(@"Resampling...", nil)];
-	[splash showWindow:self];
-	
-	resample->Update();
-	
-	float* resultBuff = resample->GetOutput()->GetBufferPointer();
-	
+            ResampleFilterType::Pointer resample = ResampleFilterType::New();
+            
+            resample->SetTransform(transform);
+            resample->SetInput([itkImage itkImporter]->GetOutput());
+            resample->SetDefaultPixelValue( [firstObjectOriginal minValueOfSeries]);
+            
+            double outputSpacing[3];
+            outputSpacing[0] = [firstObject pixelSpacingX];
+            outputSpacing[1] = [firstObject pixelSpacingY];
+            outputSpacing[2] = [firstObject sliceInterval];
+            
+            if( outputSpacing[2] == 0 || noOfImages == 1)
+                outputSpacing[2] = 1;
+            
+            resample->SetOutputSpacing(outputSpacing);
+            
+            double outputOrigin[3] = {0, 0, 0}, outputOriginConverted[3] = {0, 0, 0};
+            
+            outputOrigin[0] =  [firstObject originX];
+            outputOrigin[1] =  [firstObject originY];
+            outputOrigin[2] =  [firstObject originZ];
+            
+            outputOriginConverted[ 0] = outputOrigin[ 0] * vectorReference[ 0] + outputOrigin[ 1] * vectorReference[ 1] + outputOrigin[ 2] * vectorReference[ 2];
+            outputOriginConverted[ 1] = outputOrigin[ 0] * vectorReference[ 3] + outputOrigin[ 1] * vectorReference[ 4] + outputOrigin[ 2] * vectorReference[ 5];
+            outputOriginConverted[ 2] = outputOrigin[ 0] * vectorReference[ 6] + outputOrigin[ 1] * vectorReference[ 7] + outputOrigin[ 2] * vectorReference[ 8];
+            
+            resample->SetOutputOrigin( outputOriginConverted);
+            
+            ImageType::SizeType size;
+            size[0] = [firstObject pwidth];
+            size[1] = [firstObject pheight];
+            size[2] = noOfImages;
+            
+            resample->SetSize(size);
+            
+            if( noOfImages > 2)
+                splash = [[WaitRendering alloc] init:NSLocalizedString(@"Resampling...", nil)];
+            [splash showWindow:self];
+            
+            resample->Update();
+            
+            resultBuff = resample->GetOutput()->GetBufferPointer();
+        }
+        @catch (NSException *e)
+        {
+            N2LogException( e);
+        }
+    }
+    catch (...)
+    {
+        N2LogStackTrace( @"C++ exception");
+    }
+    
 	[splash close];
 	[splash release];
 	
@@ -142,6 +158,7 @@ typedef itk::ResampleImageFilter<ImageType, ImageType> ResampleFilterType;
 		return fVolumePtr;
 	}
 	else return nil;
+        
 }
 
 + (float*) reorient2Dimage: (double*) theParameters firstObject: (DCMPix*) firstObject firstObjectOriginal: (DCMPix*) firstObjectOriginal length: (long*) length
