637de34a4f2fce16e7a862fba8919ee65b3d044d
Antoine Rosset <rossetantoine@bluewin.ch>
2012-09-26 09:43:32 +0000

    Limit C-Find only at STUDY or SERIES level
    git-svn-id: https://svn.code.sf.net/p/osirix/code/osirix@10489 4e9e6e3d-4551-47eb-9dbe-a6127f9400a1

--- OsiriXSCPDataHandler.mm
+++ OsiriXSCPDataHandler.mm
@@ -1468,9 +1468,8 @@ extern BOOL forkedProcess;
             {
                 if( [[NSUserDefaults standardUserDefaults] integerForKey: @"maximumNumberOfCFindObjects"] > 0 && tempFindArray.count > [[NSUserDefaults standardUserDefaults] integerForKey: @"maximumNumberOfCFindObjects"])
                 {
+                    NSLog( @"----- C-Find maximumNumberOfCFindObjects reached: %d, %d", (int) tempFindArray.count, [[NSUserDefaults standardUserDefaults] integerForKey: @"maximumNumberOfCFindObjects"]);
                     tempFindArray = [tempFindArray subarrayWithRange: NSMakeRange( 0, [[NSUserDefaults standardUserDefaults] integerForKey: @"maximumNumberOfCFindObjects"])];
-                    
-                    NSLog( @"----- C-Find maximumNumberOfCFindObjects reached: %d", [[NSUserDefaults standardUserDefaults] integerForKey: @"maximumNumberOfCFindObjects"]);
                 }
             }
             
