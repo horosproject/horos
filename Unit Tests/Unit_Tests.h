b04e4eb425c6f5a09d86a2dbe4fe47d6f89057ce
Joris Heuberger <j0ris@bluewin.ch>
2013-02-25 14:54:45 +0100

    MD-37 add standard unit tests (currently failing)

---
+++ Unit Tests/Unit_Tests.h
@@ -0,0 +1,13 @@
+//
+//  Unit_Tests.h
+//  Unit Tests
+//
+//  Created by Joris Heuberger on 25.02.13.
+//  Copyright (c) 2013 OsiriX Team. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Unit_Tests : SenTestCase
+
+@end
