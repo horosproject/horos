8d405d4ab090a32cb02e640a66d1e89e8b938952
osirixexperts <danny@purview.net>
2015-02-18 11:54:07 -0500

    Updated Copyright Information

--- nitrogen/Sources/NSWindow+N2.mm
+++ nitrogen/Sources/NSWindow+N2.mm
@@ -10,6 +10,45 @@
      This software is distributed WITHOUT ANY WARRANTY; without even
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.
+ ---------------------------------------------------------------------------
+ 
+ This file is part of the Horos Project.
+ 
+ Current contributors to the project include Alex Bettarini and Danny Weissman.
+ 
+ Horos is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation,  version 3 of the License.
+ 
+ Horos is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Horos.  If not, see <http://www.gnu.org/licenses/>.
+
+ 
+
+ 
+ ---------------------------------------------------------------------------
+ 
+ This file is part of the Horos Project.
+ 
+ Current contributors to the project include Alex Bettarini and Danny Weissman.
+ 
+ Horos is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation,  version 3 of the License.
+ 
+ Horos is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Horos.  If not, see <http://www.gnu.org/licenses/>.
+
 =========================================================================*/
 
 #import "NSWindow+N2.h"
