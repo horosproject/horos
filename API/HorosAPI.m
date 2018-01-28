12386e066cf19f00f594888f1933cee5b0e52b5e
Fauze Polpeta <fauze.polpeta@gmail.com>
2018-01-28 18:19:56 -0200

    [HOR-20] Keep legacy framework tweaks in HorosAPI.m until rebuild plugins

--- API/HorosAPI.m
+++ API/HorosAPI.m
@@ -47,3 +47,7 @@ const unsigned char OsiriXAPIVersionString[] = "1.0.0";
 
 double OsiriX_HeadersVersionNumber = 100;
 const unsigned char OsiriX_HeadersVersionString[] = "1.0.0";
+
+//As this is a temporary tw=ak, this can be also here
+double HorosDCMVersionNumber = 100;
+const unsigned char HorosDCMVersionString[] = "1.0.0";
--- Horos/Scripts/Horos/Clean-Frameworks.sh
+++ Horos/Scripts/Horos/Clean-Frameworks.sh
@@ -6,7 +6,7 @@ framework_path="$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/Horos.framework"
 
 # many plugins are hard-linked to the API framework, and its name changed over time
 
-alts=( HorosAPI OsiriXAPI 'OsiriX Headers' )
+alts=( HorosAPI OsiriXAPI 'OsiriX Headers' HorosDCM)
 for alt in "${alts[@]}"; do
     alt_framework_path="$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$alt.framework"
     rm -Rf "$alt_framework_path"
@@ -17,21 +17,10 @@ for alt in "${alts[@]}"; do
     ln -s "Versions/A/$alt"
     sed -i '' "s/Horos/$alt/" "Versions/A/Resources/Info.plist"
     sed -i '' "s/org.horosproject.api/org.horosproject.$alt/" "Versions/A/Resources/Info.plist"
-    #exception since this is temporary
-    sed -i '' "s/org.horosproject.OsiriX\ Headers/org.horosproject.OsiriXHeaders/" "Versions/A/Resources/Info.plist"
 done
 
-alts=( HorosDCM )
-for alt in "${alts[@]}"; do
-    alt_framework_path="$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$alt.framework"
-    rm -Rf "$alt_framework_path"
-    cp -R "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/DCM.framework" "$alt_framework_path"
-    mv "$alt_framework_path/Versions/A/DCM" "$alt_framework_path/Versions/A/$alt"
-    rm "$alt_framework_path/DCM"
-    cd "$alt_framework_path"
-    ln -s "Versions/A/$alt"
-    sed -i '' "s/DCM/$alt/" "Versions/A/Resources/Info.plist"
-    sed -i '' "s/org.horosproject.dcm/org.horosproject.$alt/" "Versions/A/Resources/Info.plist"
-done
+#exception since this is temporary
+cd "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/OsiriX Headers.framework"
+sed -i '' "s/org.horosproject.OsiriX\ Headers/org.horosproject.OsiriXHeaders/" "Versions/A/Resources/Info.plist"
 
 exit 0
