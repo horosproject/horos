24ecf78ca039a567203ae9d8a4ee451c58d33ef3
Fauze Polpeta <fauze.polpeta@gmail.com>
2018-02-06 18:16:29 -0200

    [HOR-17] Added Grok to build system

---
+++ Horos/Scripts/Grok/Make.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e; set -o xtrace
+
+cmake_dir="$TARGET_TEMP_DIR/CMake"
+libs_dir="$cmake_dir/bin"
+
+[ -f "$libs_dir/libopenjpg2.a" ] && [ ! -f "$cmake_dir/.incomplete" ] && exit 0
+
+export CC=clang
+export CXX=clang
+
+touch "$cmake_dir/.incomplete"
+
+args=( -j 8 VERBOSE=1)
+
+cd "$cmake_dir"
+make "${args[@]}"
+make install
+
+rm -f "$cmake_dir/.incomplete"
+
+exit 0
